9e770e81ce2f91885fb415a9ab4fef3e12acb357
Fahd Ahsan <sfahsan@umich.edu>
2022-12-15 18:03:40 -0500

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,6 +31,15 @@ router.post(
     }
 );
 
+//Log out
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
 
 
 module.exports = router;
