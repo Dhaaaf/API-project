b52f8918fc9c3baef6a55330f410838d95dccca0
Fahd Ahsan <sfahsan@umich.edu>
2022-12-16 15:50:04 -0500

    Fixing render deployment

--- backend/db/seeders/20221215190054-demo-user.js
+++ backend/db/seeders/20221215190054-demo-user.js
@@ -1,8 +1,6 @@
 'use strict';
 const bcrypt = require("bcryptjs");
 
-/** @type {import('sequelize-cli').Migration} */
-
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
