cf6dbe26cb9e5be23ebf64628a2a6fac230c0650
Fahd Ahsan <sfahsan@umich.edu>
2022-12-17 02:20:52 -0500

    Added reviewImages

---
+++ backend/db/migrations/20221217071148-create-review-image.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ReviewImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Reviews' }
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "ReviewImages"
+    await queryInterface.dropTable(options);
+  }
+};
