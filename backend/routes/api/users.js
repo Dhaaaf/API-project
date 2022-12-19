237e30580fe7efbdf95242c56f5c027a5a226a7c
Fahd Ahsan <sfahsan@umich.edu>
2022-12-19 17:43:18 -0500

    small change

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,6 @@
 // backend/routes/api/users.js
 const express = require('express');
+const router = express.Router();
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
@@ -7,7 +8,6 @@ const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
-const router = express.Router();
 
 
 const validateSignup = [
