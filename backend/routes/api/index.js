cfbe5af4833dfe68bf38cf40337c06be7db0bfeb
Fahd Ahsan <sfahsan@umich.edu>
2022-12-21 12:42:31 -0500

    Done with get booking by current user

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
+const bookingsRouter = require('./bookings.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -15,6 +16,7 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
