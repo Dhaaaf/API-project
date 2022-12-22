349c7ad5ba71405baef6f263a798aedcea946d0d
Fahd Ahsan <sfahsan@umich.edu>
2022-12-21 19:52:46 -0500

    Delet spot and review images

---
+++ backend/routes/api/review-images.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User, Spot, Review, SpotImage, ReviewImage, Booking } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const sequelize = require('sequelize');
+
+// Delete a review Image
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    const { imageId } = req.params;
+    const user = req.user
+
+    const image = await ReviewImage.findByPk(imageId);
+
+    const err = {};
+    if (!image) {
+        err.title = "Couldn't find a Review Image with the specified id";
+        err.status = 404;
+        err.message = "Review Image couldn't be found"
+        return next(err)
+    };
+
+    const review = await image.getReview();
+    if (user.id !== review.userId) {
+        err.title = "Authorization error";
+        err.status = 403;
+        err.message = "Cannot delete image from review not left by user"
+        return next(err)
+    };
+
+    image.destroy();
+    res.json({
+        "message": "Successfully deleted",
+        "statusCode": 200
+    })
+})
+
+
+
+
+module.exports = router;
