6a4e321c081624134d1c25000c41bbd135a445e6
Fahd Ahsan <sfahsan@umich.edu>
2023-01-13 00:02:00 -0500

    Done with sign-up

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -42,6 +42,23 @@ export const restoreUser = () => async dispatch => {
     return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
