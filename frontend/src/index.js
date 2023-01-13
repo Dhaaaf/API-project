155bbca58c50624248ff0bf654a097ec546cd9f7
Fahd Ahsan <113646829+Dhaaaf@users.noreply.github.com>
2023-01-13 00:04:37 -0500

    Revert "Dev"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,17 +5,11 @@ import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
-  restoreCSRF();
-
-  window.csrfFetch = csrfFetch;
+if (process.env.NODE_ENV !== "production") {
   window.store = store;
-  window.sessionActions = sessionActions;
 }
 
 function Root() {
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,8 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import sessionReducer from './session';
 
 const rootReducer = combineReducers({
   // add reducer functions here
-  session: sessionReducer,
 });
 
 let enhancer;
