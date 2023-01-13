d443526e8fd08d44d28cb61527d172d75974387a
Fahd Ahsan <sfahsan@umich.edu>
2023-01-13 17:59:56 -0500

    example push

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -4,6 +4,7 @@ import * as sessionActions from '../../store/session';
 import OpenModalMenuItem from './OpenModalMenuItem';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
+import { Link } from "react-router-dom";
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
