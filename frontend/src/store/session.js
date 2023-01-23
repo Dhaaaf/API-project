30d5c60ea1dbdea64b30d018391712e575b09dc8
Fahd Ahsan <sfahsan@umich.edu>
2023-01-22 23:10:18 -0500

    fixed a few bugs

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -55,7 +55,8 @@ export const signup = (user) => async (dispatch) => {
         }),
     });
     const data = await response.json();
-    dispatch(setUser(data.user));
+    console.log("Data......", data)
+    dispatch(setUser(data.user.user));
     return response;
 };
 
